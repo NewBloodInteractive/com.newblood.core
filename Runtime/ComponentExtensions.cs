36f13dd9d875dcf9ac1aa7aa9ec8dd0a38c10869
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2022-01-04 02:03:28 +1000

    Add ComponentExtensions and GameObjectExtensions

---
+++ Runtime/ComponentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace NewBlood
+{
+    /// <summary>Provides extension methods for <see cref="Component"/>.</summary>
+    public static class ComponentExtensions
+    {
+        /// <summary>Returns the component of type <typeparamref name="T"/> if the game object has one attached, adds one if it doesn't.</summary>
+        public static T GetOrAddComponent<T>(this Component @this)
+            where T : Component
+        {
+            if (@this.TryGetComponent(out T component))
+                return component;
+
+            return @this.gameObject.AddComponent<T>();
+        }
+
+        /// <summary>Returns the component of type <paramref name="type"/> if the game object has one attached, adds one if it doesn't.</summary>
+        public static Component GetOrAddComponent(this Component @this, Type type)
+        {
+            if (@this.TryGetComponent(type, out Component component))
+                return component;
+
+            return @this.gameObject.AddComponent(type);
+        }
+    }
+}
