8fe47c2580d4bee24dd869a46e57743e9c518622
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2022-01-21 00:28:50 +1000

    Implement MainThread.Send and MainThread.Post

--- Runtime/MainThread.cs
+++ Runtime/MainThread.cs
@@ -15,6 +15,18 @@ namespace NewBlood
         /// <summary>Gets the synchronization context for the main thread.</summary>
         public static SynchronizationContext SynchronizationContext { get; private set; }
 
+        /// <summary>Dispatches a synchronous message to the main thread.</summary>
+        public static void Send(SendOrPostCallback callback, object state)
+        {
+            SynchronizationContext.Send(callback, state);
+        }
+
+        /// <summary>Dispatches an asynchronous message to the main thread.</summary>
+        public static void Post(SendOrPostCallback callback, object state)
+        {
+            SynchronizationContext.Post(callback, state);
+        }
+
         /// <summary>Initializes the <see cref="MainThread"/> class.</summary>
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         static void OnSubsystemRegistration()
